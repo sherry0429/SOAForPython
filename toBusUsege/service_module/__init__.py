b846cc825e5f7c5dc3c5b6c6c5fb06a009a4c9a5
Sherr <sherry0429@outlook.com>
2018-02-02 18:07:21 +0800

    remove some files

---
+++ toBusUsege/service_module/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2017 tianyou pan <sherry0429 at SOAPython>
+"""
+from service_core import ServiceEngineModule
+from service_core import ServiceParamTemplate
+
+__all__ = ['ServiceEngineModule', 'ServiceParamTemplate']
